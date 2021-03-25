ef6ebc6c2cb97f82aa1ce97e1744967147088403
Bill <reborn800201@gmail.com>
2021-03-25 18:43:54 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 
-import { getProducts, createProduct, postProduct, getProduct, editProduct, putProduct, deleteProduct, getUserslist, putUsersadmin } from '../controllers/adminController.js'
-import { getProducts as _getProducts, getFeeds, getTopProducts, getProduct as _getProduct, getDashboard } from '../controllers/productController.js'
-import { getCategories, postCategory, putCategory, deleteCategory } from '../controllers/categoryController.js'
-import { getCart, postCart, addCartItem, subCartItem, deleteCartItem } from '../controllers/cartController.js'
-import { signUpPage, signUp, signInPage, signIn, logout, addFavorite, removeFavorite, getUser, editUser, putUser } from '../controllers/userController.js'
-import { getOrders, postOrder, cancelOrder, getOrder, getPayment, newebpayCallback } from '../controllers/orderController.js'
-import { postComment, deleteComment } from '../controllers/commentController.js'
+let adminController = require('../controllers/adminController.js')
+let productController = require('../controllers/productController.js')
+let categoryController = require('../controllers/categoryController.js')
+let cartController = require('../controllers/cartController.js')
+let userController = require('../controllers/userController.js')
+let orderController = require('../controllers/orderController.js')
+let commentController = require('../controllers/commentController.js')
 
-import multer from 'multer'
+const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
-import { authenticate } from '../config/passport'
+const passport = require('../config/passport')
 
 const authenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -26,69 +26,69 @@ const authenticatedAdmin = (req, res, next) => {
 }
 
 
-export default app => {
+module.exports = app => {
 
-  app.get('/signup', signUpPage)
-  app.post('/signup', upload.single('image'), signUp)
-  app.get('/signin', signInPage)
-  app.post('/signin', authenticate('local', { failureRedirect: '/signin', failureFlash: true }), signIn)
-  app.get('/logout', logout)
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', upload.single('image'), userController.signUp)
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+  app.get('/logout', userController.logout)
 
   //custom
   app.get('/', (req, res) => res.redirect('/products'))
-  app.get('/products', _getProducts)
-  app.get('/products/feeds', getFeeds)
-  app.get('/products/top', getTopProducts)
-  app.get('/products/:id', _getProduct)
-  app.get('/products/:id/dashboard', getDashboard)
+  app.get('/products', productController.getProducts)
+  app.get('/products/feeds', productController.getFeeds)
+  app.get('/products/top', productController.getTopProducts)
+  app.get('/products/:id', productController.getProduct)
+  app.get('/products/:id/dashboard', productController.getDashboard)
   // authenticated,
 
   // admin
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/products'))
-  app.get('/admin/products', authenticatedAdmin, getProducts)
-  app.get('/admin/products/create', authenticatedAdmin, createProduct)
-  app.post('/admin/products', authenticatedAdmin, upload.single('image'), postProduct)
-  app.get('/admin/products/:id', authenticatedAdmin, getProduct)
-  app.get('/admin/products/:id/edit', authenticatedAdmin, editProduct)
-  app.put('/admin/products/:id', authenticatedAdmin, upload.single('image'), putProduct)
-  app.delete('/admin/products/:id', authenticatedAdmin, deleteProduct)
+  app.get('/admin/products', authenticatedAdmin, adminController.getProducts)
+  app.get('/admin/products/create', authenticatedAdmin, adminController.createProduct)
+  app.post('/admin/products', authenticatedAdmin, upload.single('image'), adminController.postProduct)
+  app.get('/admin/products/:id', authenticatedAdmin, adminController.getProduct)
+  app.get('/admin/products/:id/edit', authenticatedAdmin, adminController.editProduct)
+  app.put('/admin/products/:id', authenticatedAdmin, upload.single('image'), adminController.putProduct)
+  app.delete('/admin/products/:id', authenticatedAdmin, adminController.deleteProduct)
 
 
 
-  app.get('/admin/categories', authenticatedAdmin, getCategories)
-  app.post('/admin/categories', authenticatedAdmin, postCategory)
-  app.get('/admin/categories/:id', authenticatedAdmin, getCategories)
-  app.put('/admin/categories/:id', authenticatedAdmin, putCategory)
-  app.delete('/admin/categories/:id', authenticatedAdmin, deleteCategory)
+  app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+  app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
+  app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
+  app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+  app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 
 
-  app.get('/admin/users', authenticatedAdmin, getUserslist)
-  app.put('/admin/users/:id', authenticatedAdmin, putUsersadmin)
+  app.get('/admin/users', authenticatedAdmin, adminController.getUserslist)
+  app.put('/admin/users/:id', authenticatedAdmin, adminController.putUsersadmin)
 
 
-  app.get('/cart', authenticated, getCart)
-  app.post('/cart', authenticated, postCart)
-  app.post('/cartItem/:id/add', authenticated, addCartItem)
-  app.post('/cartItem/:id/sub', authenticated, subCartItem)
-  app.delete('/cartItem/:id', authenticated, deleteCartItem)
+  app.get('/cart', authenticated, cartController.getCart)
+  app.post('/cart', authenticated, cartController.postCart)
+  app.post('/cartItem/:id/add', authenticated, cartController.addCartItem)
+  app.post('/cartItem/:id/sub', authenticated, cartController.subCartItem)
+  app.delete('/cartItem/:id', authenticated, cartController.deleteCartItem)
 
 
-  app.get('/orders', authenticated, getOrders)
-  app.post('/order', authenticated, postOrder)
-  app.post('/order/:id/cancel', authenticated, cancelOrder)
-  app.get('/orders/:id', authenticated, getOrder)
-  app.get('/order/:id/payment', getPayment)
-  app.post('/newebpay/callback', newebpayCallback)
+  app.get('/orders', authenticated, orderController.getOrders)
+  app.post('/order', authenticated, orderController.postOrder)
+  app.post('/order/:id/cancel', authenticated, orderController.cancelOrder)
+  app.get('/orders/:id', authenticated, orderController.getOrder)
+  app.get('/order/:id/payment', orderController.getPayment)
+  app.post('/newebpay/callback', orderController.newebpayCallback)
 
-  app.post('/comments', authenticated, postComment)
-  app.delete('/comments/:id', authenticatedAdmin, deleteComment)
+  app.post('/comments', authenticated, commentController.postComment)
+  app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 
-  app.post('/favorite/:productId', authenticated, addFavorite)
-  app.delete('/favorite/:productId', authenticated, removeFavorite)
+  app.post('/favorite/:productId', authenticated, userController.addFavorite)
+  app.delete('/favorite/:productId', authenticated, userController.removeFavorite)
 
 
-  app.get('/users/:id', authenticated, getUser)
-  app.get('/users/:id/edit', authenticated, editUser)
-  app.put('/users/:id', authenticated, upload.single('image'), putUser)
+  app.get('/users/:id', authenticated, userController.getUser)
+  app.get('/users/:id/edit', authenticated, userController.editUser)
+  app.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 } 
